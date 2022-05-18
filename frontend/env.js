7fb51b1fb1418149547b37aaf46c0d626d0ed58f
Nikita <user@MacBook-Pro-user.local>
2022-05-18 21:56:45 +0300

    fix bug

--- frontend/env.js
+++ frontend/env.js
@@ -1,2 +1,2 @@
-export const API_URL_SERVER = 'https://' + 'backend:8000' + '/api' 
+export const API_URL_SERVER = 'http://' + 'backend:8000' + '/api' 
 export const API_URL = 'https://' + 'speclg.ru' + '/api' 
