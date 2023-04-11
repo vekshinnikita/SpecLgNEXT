ff7b527104f2ac417f4c3b69f57cca3e2169500a
Nikita <user@MacBook-Pro-user.local>
2023-04-11 17:06:13 +0300

    fix

--- TelegramBotVasya/user_bot.py
+++ TelegramBotVasya/user_bot.py
@@ -14,6 +14,7 @@ session_maker = get_session_maker(db_engine)
 
 @app.on_message()
 async def hello(client, message: Message):
+    print('hello')
     if not message.outgoing:
         me = await app.get_users('me')
         with session_maker() as session:
